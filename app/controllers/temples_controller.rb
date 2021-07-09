dc51ddd4c85f1a7006daeea15c369f0952a2aa77
mai-yamato <my512.1515@gmail.com>
2021-07-09 07:16:23 +0000

    temple index 追加

--- app/controllers/temples_controller.rb
+++ app/controllers/temples_controller.rb
@@ -1,6 +1,7 @@
 class TemplesController < ApplicationController
 
   def index
+    @temples = Temple.all
   end
 
   def show
@@ -10,7 +11,22 @@ class TemplesController < ApplicationController
   end
 
 
+
+  def create
+    @temple = Temple.new(temples_params)
+    if @temple.save
+      redirect_to temples_path, notice: 'セーブ'
+    else
+      render temples_path
+    end
+  end
+
+  private
+
   def update
   end
 
+  def temples_params
+    params.permit(:prefecture, :temple_number, :temple_name, :temple_image)
+  end
 end
