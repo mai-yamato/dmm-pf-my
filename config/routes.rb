350ebe15938c6e8be7548902a25d992f18e030ba
mai-yamato <my512.1515@gmail.com>
2021-07-11 14:41:22 +0000

    user view追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get '/home/about' => 'homes#about'
-  resources :users, :only => [:index, :show, :edit, :update]
-  resources :temples, :only => [:index, :show, :edit, :update, :create]
+  resources :users, :only => [:index, :show, :create, :edit, :update]
+  resources :temples, :only => [:index, :show, :create, :edit, :update]
 end
